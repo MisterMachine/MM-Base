5bb9a357662c950bb803711065695906b5d3ab74
menslow <michael.enslow@gmail.com>
2011-01-05 17:24:49 -0800

    Added Conditional CSS class and Modernizr

--- functions.php
+++ functions.php
@@ -1,4 +1,45 @@
 <?php
 require_once('classes/mmbase.php');
 MMBase::init();
+
+/**
+* add_site_styles function.
+* Add site styles to the head of the theme
+* @access public
+* @return void
+*/	
+function add_site_styles(){
+
+	wp_enqueue_style('reset', CSS . '/reset.css');	
+	wp_enqueue_style('text', CSS . '/text.css');
+	wp_enqueue_style('960', CSS . '/960.css');
+	wp_enqueue_style('theme', CSS . '/theme.css');
+}
+
+/**
+ * add_site_scripts function.
+ * Add site scripts to the head of the theme
+ * @access public
+ * @return void
+ */
+function add_site_scripts() {
+
+	if (!is_admin()) {
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'modernizr', JS . '/modernizr-1.6.min.js', array('jquery') );
+		wp_enqueue_script( 'app', JS . '/app.js', array('jquery') );
+	}
+
+}
+
+/**
+ * new_excerpt_more function.
+ * Over write the default WordPres [...] with anything you like.
+ * @access public
+ * @return void
+ */
+function new_excerpt_more($more) {
+	return '...';
+}
+
 ?>
--- header.php
+++ header.php
@@ -4,24 +4,28 @@
  * @subpackage mm-base
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
+<!doctype html>
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-<link rel="stylesheet" href="<?php echo CSS . '/reset.css'; ?>" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php echo CSS . '/text.css'; ?>" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php echo CSS . '/960.css'; ?>" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php echo CSS . '/theme.css'; ?>" type="text/css" media="screen" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 	<div id="page" class="wrapper">
 
